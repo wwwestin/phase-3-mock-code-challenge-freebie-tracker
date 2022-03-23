fb374b5b531f365a8dd6e9f21d3c4409c897544b
wwwestin <westin.h@gmail.com>
2022-03-23 11:06:54 -0400

    First attempt

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,8 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
-
+Freebie.create(item_name: "Coke Can", value: 3, dev_id: 1, company_id: 1)
+Freebie.create(item_name: "A Quarter", value: 2, dev_id: 2, company_id: 2)
+Freebie.create(item_name: "The Kramer", value: 1, dev_id: 3, company_id: 3)
+Freebie.create(item_name: "The Jerry", value: 6, dev_id: 4, company_id: 4)
 puts "Seeding done!"
